ec8a6b5542d855f4826196c665cba24bdb748a6f
laska01 <lifejanek@gmail.com>
2026-03-13 12:00:16 +0100

    stoper tylko dziala

--- app/src/main/java/com/example/timery/MainActivity.java
+++ app/src/main/java/com/example/timery/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.timery;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int ileSekund;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        Handler handler = new Handler();
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        ileSekund++;
+                        textView.setText(""+ileSekund);
+                        handler.postDelayed(this,1000);
+
+                    }
+                }
+        );
 
 
     }
