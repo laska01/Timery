d514abdab4ef544b5d5e643fd910993cf9c1ab26
laska01 <lifejanek@gmail.com>
2026-03-13 12:08:50 +0100

    stopowania i start czasu

--- app/src/main/java/com/example/timery/MainActivity.java
+++ app/src/main/java/com/example/timery/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.timery;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +14,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private int ileSekund;
+    Button start;
+    Button stop;
+    Button reset;
+    Button zapisz;
+
+    private boolean czyIdzie = false;
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,35 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
+        start.findViewById(R.id.start);
+        stop.findViewById(R.id.stop);
+        reset.findViewById(R.id.reset);
+        zapisz.findViewById(R.id.zapisz);
         Handler handler = new Handler();
+        start.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        czyIdzie = true;
+                    }
+                }
+        );
+        stop.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        czyIdzie = false;
+                    }
+                }
+        );
         handler.post(
                 new Runnable() {
                     @Override
                     public void run() {
-                        ileSekund++;
-                        textView.setText(""+ileSekund);
+                        if (czyIdzie) {
+                            ileSekund++;
+                            textView.setText("" + ileSekund);
+                        }
                         handler.postDelayed(this,1000);
 
                     }
